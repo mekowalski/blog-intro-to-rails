2bf9893090621bce74fadd452406708824c6b3ac
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 15:42:35 -0600

    modify routes with a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'goodbye/index'
   get 'welcome/index'
+
+  root'welcome#index'
+  # tells rails to go to welcome controller and index action
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
